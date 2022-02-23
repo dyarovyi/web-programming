2f6969f7d47a72310bbeebdc264457dc1cd0d6f8
Daniil Yarovyi <daniel.yarovy@gmail.com>
2022-02-23 16:00:12 +0200

    lab1-practice

---
+++ Lab-1/Practice/23-02-2022/script.js
@@ -0,0 +1,9 @@
+function button_click() {
+    const li = document.createElement("li")
+    li.innerHTML = 'hey'
+    document.getElementById('for_script').appendChild(li)
+
+    
+    //element.innerHTML = 'text'
+    // document.getElementsById('html').appendChild(element)
+}
